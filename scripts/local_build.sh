65be15aac808bca572d80002897e3fb42d98f4d1
Stephan Borg <wolffborg1@gmail.com>
2025-01-16 09:00:44 +1100

    Initial release v0.1.0

--- scripts/local_build.sh
+++ scripts/local_build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# python -m pip install --upgrade pip
-# python -m pip install --upgrade virtualenv
-# python -m venv .venv
-# source .venv/Scripts/activate
 python -m pip install --upgrade pip
 pip install --upgrade virtualenv
 pip install --upgrade poetry
@@ -20,12 +16,10 @@ poetry install --sync # --without=dev
 poetry run mypy process_analyst_copilot tests
 poetry run flake8 #--output-file=build/flake8/flake8.txt
 poetry run pytest --cov=process_analyst_copilot --cov-report=term-missing --cov-report=html:build/coverage-reports
-poetry run behave
+#poetry run behave
 poetry run pdoc --output-dir build/pdoc process_analyst_copilot
 
 poetry run cz bump --changelog --yes
 poetry build
 
 # poetry run twine check dist/*
-
-deactivate
